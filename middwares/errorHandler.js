c643a2250b3681a4c0f7b77b1bdcdee3da81abef
Yohpram <126755927+Yohpram@users.noreply.github.com>
2023-10-18 09:13:35 +0700

    second commit

--- middwares/errorHandler.js
+++ middwares/errorHandler.js
@@ -1,1 +1,17 @@
-module.exports
+module.exports = {
+    errorHandlers: (err, req, res, next) => {
+        if(err){
+            switch (err.name){
+                case "SignInError":
+                    res.status(401).json({massage: 'invalid username/password' })
+                case "Unauthorize":
+                    res.status(401).json({massage: 'you dont have access'})
+                default:
+                    res.status(500).json({massagr: 'internal server error'})
+            }
+        }
+    }
+
+    
+
+}
