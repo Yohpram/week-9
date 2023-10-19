1b26d678f28337eb2700ce26bb79a31956169fd0
Yohpram <126755927+Yohpram@users.noreply.github.com>
2023-10-19 11:52:57 +0700

    second commit

--- middwares/auth.js
+++ middwares/auth.js
@@ -4,15 +4,23 @@ const pool = require('../query');
 module.exports = {
   authenticate: async function (req, res, next) {
     try {
+
+      // Periksa apakah rute yang sedang diakses adalah rute dokumentasi Swagger
+      if (req.originalUrl === '/api-docs' || req.originalUrl.startsWith('/api-docs/')) {
+        // Jika rute adalah rute dokumentasi Swagger, maka lanjutkan tanpa otentikasi
+        return next();
+      }
+
       const accessToken = req.headers.access_token
 
       const decoded = verifyToken(accessToken)
 
       const checkUser = await pool.query(`SELECT * FROM public.users WHERE email = '${decoded.email}' AND password = '${decoded.password}'`);
 
-      console,log(checkUser.rows[0].role, '====>')
+      console.log(checkUser.rows[0].role,'====>')
+
       if (checkUser) {
-        req.role = checkUser.rows,[0].role;
+        req.role = checkUser.rows[0].role;
         next()
       } else {
         next({name: 'SignInerror'})
@@ -23,7 +31,7 @@ module.exports = {
   },
   authorize: async function (req, res, next) { 
     try{
-    const isSupervisor = req.role === 'Supervisor'
+    const isSupervisor = req.role === 'Engineer'
 
     if(!isSupervisor){
         throw new Error;
