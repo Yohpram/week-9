2cc388019e16c938f5cf9e23e447275e1a8994b5
Yohpram <126755927+Yohpram@users.noreply.github.com>
2023-10-19 11:51:26 +0700

    third commit

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express')
 const pool = require('./query.js')
-const bodyParser = require('body-parser')
 require("dotenv").config();
+const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express');
 const { authenticate } = require('./middwares/auth.js')
+
 
 const app = express()
 const port = 3002
 
 const movieRouter = require('./movieRouter.js')
 const userRouter = require('./userRouter.js')
+const swaggerdoc = require('./sweggerdoc.json');
 
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
 
 
 app.use('/users', userRouter);
-//app.use(authenticate);
+app.use(authenticate);
 app.use('/movies', movieRouter);
-
+// Rute untuk dokumentasi Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerdoc));
 
 app.listen(port, () => {
   console.log(`running on port ${port}`)
